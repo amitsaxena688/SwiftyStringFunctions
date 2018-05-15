f88b92a14d7340a92584063dc32214f64d40b7af
Amit Saxena <>
2018-05-15 19:59:40 +0530

    initial version of string helper functions

---
+++ StringFunctions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StringFunctions
+//
+//  Created by Amit Saxena on 15/05/18.
+//  Copyright © 2018 Amit Saxena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
